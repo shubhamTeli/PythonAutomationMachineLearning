90fed808db07ee658ee01bda6c3deccce7666929
Shubham Teli <57106568+shubhamTeli@users.noreply.github.com>
2019-11-10 16:03:03 +0530

    Assignment 2

---
+++ Arithmatic.py
@@ -0,0 +1,11 @@
+def Add(no1, no2):
+    print("Addition is ", no1 + no2);
+
+def Sub(no1, no2):
+    print("Substraction is ", no1 - no2);
+
+def Mult(no1, no2):
+    print("Multiplication is ", no1 * no2);
+    
+def Div(no1, no2):
+    print("Division is ", no1 / no2);
