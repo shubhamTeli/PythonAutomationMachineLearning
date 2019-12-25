0161cdf36cd071626d4cb7518ad9a417ed9135b1
Shubham Teli <57106568+shubhamTeli@users.noreply.github.com>
2019-12-25 16:47:43 +0530

    User defined Module to check Internet Connection

---
+++ checkInternet.py
@@ -0,0 +1,11 @@
+from pip._vendor import requests
+
+
+def check_internet():
+    url='http://www.google.com/'
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        return False
